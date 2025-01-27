4243a9849e6a0e454046b9c140351b0fab851cac
Andrei <sharov89@gmail.com>
2025-01-27 03:42:17 +0300

    task done

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/filter/FilterContactUiState.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/filter/FilterContactUiState.java
@@ -5,7 +5,23 @@ import java.util.Set;
 
 import ru.yandex.practicum.contacts.model.ContactType;
 
-public class FilterContactUiState {
-    public boolean isApplyEnable = false;
-    public Set<ContactType> newSelectedContactTypes = Collections.emptySet();
+class FilterContactUiState {
+    private boolean isApplyEnable = false;
+    private Set<ContactType> newSelectedContactTypes = Collections.emptySet();
+
+    public boolean getIsApplyEnable() {
+        return this.isApplyEnable;
+    }
+
+    public Set<ContactType> getNewSelectedContactTypes() {
+        return this.newSelectedContactTypes;
+    }
+
+    public void setIsApplyEnable(boolean flag) {
+        this.isApplyEnable = flag;
+    }
+
+    public void setNewSelectedContactTypes(Set<ContactType> type) {
+        this.newSelectedContactTypes = type;
+    }
 }
--- app/src/main/java/ru/yandex/practicum/contacts/presentation/sort/SortUiState.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/sort/SortUiState.java
@@ -1,11 +1,26 @@
 package ru.yandex.practicum.contacts.presentation.sort;
 
 // класс UiState должен иметь package-private доступ
-public class SortUiState {
+class SortUiState {
 
     // сделайте поля isApplyEnable и newSelectedSortType приватными
-    public boolean isApplyEnable = false;
-    public String newSelectedSortType = null;
+    private boolean isApplyEnable = false;
+    private String newSelectedSortType = null;
 
     // реализуйте get и set методы для обоих полей
+    public boolean getIsApplyEnable() {
+        return this.isApplyEnable;
+    }
+
+    public String getNewSelectedSortType() {
+        return this.newSelectedSortType;
+    }
+
+    public void setIsApplyEnable(boolean flag) {
+        this.isApplyEnable = flag;
+    }
+
+    public void setNewSelectedSortType(String type) {
+        this.newSelectedSortType = type;
+    }
 }
